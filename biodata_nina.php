4308a11a2b49f117355807b7c295209527dfa6ac
NinaAmaliaKartika <55771000+NinaAmaliaKartika@users.noreply.github.com>
2020-04-16 10:05:46 +0700

    Add files via upload

---
+++ biodata_nina.php
@@ -0,0 +1,6 @@
+<?php
+echo 'Nama : Nina Amalia Kartika <br>' ;
+echo 'Kelas : XI RPL 1 <br>';
+echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
+echo 'Alamat : Jl. Sumur Bandung 3 Rt 07 RW 07 Harjamukti Cimanggis Depok  <br>';
+?>
